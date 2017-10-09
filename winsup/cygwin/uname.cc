b85b684cd415912660c36e978426c30d5739cfbf
Michael Haubenwallner <michael.haubenwallner@ssi-schaefer.com>
2017-10-09 18:43:56 +0200

    release cygwin-2.9.0-gentoo-r2

--- winsup/cygwin/uname.cc
+++ winsup/cygwin/uname.cc
@@ -40,7 +40,7 @@ uname (struct utsname *name)
       cygwin_gethostname (name->nodename, sizeof (name->nodename) - 1);
 
       /* Cygwin dll release */
-      __small_sprintf (name->release, "%d.%d.%d-gentoo-r1",
+      __small_sprintf (name->release, "%d.%d.%d-gentoo-r2",
 		       cygwin_version.dll_major / 1000,
 		       cygwin_version.dll_major % 1000,
 		       cygwin_version.dll_minor) ||
